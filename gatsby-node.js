03c38973c0f59d701193026ef377127bda1c4216
Jules Guesnon <jules.guesnon@hetic.net>
2019-11-02 17:03:22 +0100

    Fix(Config): test commit to the branch

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,7 +3,6 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
-
 // You can delete this file if you're not using it
 const path = require("path")
 const categories = require("./static/categories.json")
