93ef77f34cf346b7b8c669e06c56e6a56602f479
Jules Guesnon <jules.guesnon@hetic.net>
2019-12-10 16:33:07 +0100

    fix(Style): fixed style loading issue

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,10 @@
  */
 
 // You can delete this file if you're not using it
+const ReactDOM = require("react-dom")
+
+export function replaceHydrateFunction() {
+  return (element, container, callback) => {
+    ReactDOM.render(element, container, callback)
+  }
+}
