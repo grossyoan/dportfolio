82e1859e3ee2dd366399c28a4230f28ec55849b4
Jules Guesnon <jules.guesnon@hetic.net>
2019-12-10 16:33:48 +0100

    fix(Animations): fixed animation on refresh

--- src/components/Menu/ImageCard.js
+++ src/components/Menu/ImageCard.js
@@ -1,46 +1,27 @@
-import React from "react"
+import React, { useEffect } from "react"
 import PropTypes from "prop-types"
 import { styled } from "styletron-react"
+import { useState } from "react"
 
 const StyledImageCard = styled(
   "img",
-  ({ $theme, animationdelay, heightanimation, src, ishovered, ...props }) => ({
-    position: "absolute",
-    left: "0",
-    bottom: "0",
-    width: heightanimation ? "100%" : "0%",
-    height: heightanimation ? "0%" : "100%",
-    objectFit: "cover",
-    transform: ishovered ? "scale(1.05)" : "scale(1)",
-    opacity: ishovered ? "0.5" : "1",
-    transition: "transform 2s ease, opacity 1s ease",
-    transitionDelay: "0.05s",
-    animationDuration: ".5s",
-    animationDelay: animationdelay,
-    animationFillMode: "forwards",
-    animationTimingFunction: "ease",
-    animationName: heightanimation
-      ? {
-          from: {
-            height: "0%",
-            // transform: "translateY(100%) scale(1)"
-          },
-          to: {
-            height: "100%",
-            // transform: "translateY(0%) scale(1)"
-          },
-        }
-      : {
-          from: {
-            width: "0%",
-            // transform: "translateX(-100%) scale(1)"
-          },
-          to: {
-            width: "100%",
-            // transform: "translateX(0%) scale(1)"
-          },
-        },
-  })
+  ({ $theme, animationdelay, heightanimation, src, ishovered, ...props }) => {
+    const style = {
+      position: "absolute",
+      left: "0",
+      bottom: "0",
+      width: heightanimation ? "100%" : "0%",
+      height: heightanimation ? "0%" : "100%",
+      objectFit: "cover",
+      transform: ishovered ? "scale(1.05)" : "scale(1)",
+      opacity: ishovered ? "0.5" : "1",
+      transition: `transform 2s ease 0.05s, opacity 1s ease 0.05s, ${
+        heightanimation ? "height" : "width"
+      } .5s ease`,
+    }
+
+    return style
+  }
 )
 
 const ImageCard = ({
@@ -50,12 +31,22 @@ const ImageCard = ({
   ishovered,
   ...props
 }) => {
+  let [size, setSize] = useState("0%")
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setSize("100%")
+    }, animationdelay * 1000)
+
+    return () => clearTimeout(timeoutId)
+  }, [])
+
   return (
     <StyledImageCard
       ishovered={ishovered}
       heightanimation={heightanimation}
       animationdelay={animationdelay}
       src={src}
+      $style={{ [`${heightanimation ? "height" : "width"}`]: size }}
     />
   )
 }
