f25276644cb8cd4d92a4a06020255c09802d8aa9
Jules Guesnon <jules.guesnon@hetic.net>
2019-12-13 12:25:07 +0100

    feat(Image): added fade on loading images

--- src/components/Category/GalleryImage.js
+++ src/components/Category/GalleryImage.js
@@ -1,13 +1,33 @@
-import styled from "@emotion/styled"
+import React, { useEffect, useRef, useState } from "react"
+import { css } from "@emotion/core"
 
-const GalleryImage = styled.img`
-  object-fit: cover;
-  width: auto;
-  height: 100%;
-  @media screen and (max-width: 880px) {
-    width: 100%;
-    height: auto;
-  }
-`
+export default ({ src, alt }) => {
+  const [isLoaded, setIsLoaded] = useState(false)
+  const ref = useRef(undefined)
+  useEffect(() => {
+    const cb = () => {
+      setIsLoaded(true)
+    }
+    ref.current.addEventListener("load", cb)
 
-export default GalleryImage
+    return () => ref.current.removeEventListener("load", cb)
+  }, [])
+  return (
+    <img
+      ref={ref}
+      src={src}
+      alt={alt}
+      css={css`
+        object-fit: cover;
+        width: auto;
+        height: 100%;
+        @media screen and (max-width: 880px) {
+          width: 100%;
+          height: auto;
+        }
+        transition: opacity, 0.5s ease;
+        opacity: ${isLoaded ? 1 : 0};
+      `}
+    />
+  )
+}
